356e80ee98ae7d0d2a297466d5073c9e6786a792
Harish <harish.verma1231@gmail.com>
2023-09-22 23:01:54 +0530

    fixed warnings

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import logo from "../../assets/Artistic-logo2.png"
+import logo from "../../assets/artistic-logo2.png"
 function Header(){
     return (
         <header className="header">
