e00a47ec7dfb661da69b38f7cf4b4c32c5a11770
Harish <harish.verma1231@gmail.com>
2023-10-05 15:30:44 +0530

    updated github

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ function Footer(){
     return (
         <footer className="footer">
             <div className="footer_links">
-                <a href="https://github.com/harishverma1231" target="_blank"><i className="fa-brands fa-square-github fa-2xl"></i></a>
+                <a href="https://github.com/vermaharish1231" target="_blank"><i className="fa-brands fa-square-github fa-2xl"></i></a>
                 <a href="mailto:harish.verma1231@gmail.com" target="_blank"><i className="fa-solid fa-envelope fa-2xl"></i></a>
                 <a href="https://www.instagram.com/hvrajput7" target="_blank"><i className="fa-brands fa-instagram fa-2xl"></i></a>
             </div>
