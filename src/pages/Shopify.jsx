fc3ada8a8e010edad046ded63d8457a88b395dbc
Harish Verma <92635837+vermaharish1231@users.noreply.github.com>
2024-04-16 13:22:47 +0530

    Updated Server Path Url.jsx

--- src/pages/Shopify.jsx
+++ src/pages/Shopify.jsx
@@ -73,7 +73,7 @@ const Shopify = () => {
       setproduct(data.find(product=>product.id===id));
     }
     useEffect(() => {       
-       fetch("https://demo.rejoincycles.com/tetsing/curl11.php", { 
+       fetch("https://demo.rejoincycles.com/chatbot/curl11.php", { 
          method: "GET"
        })
        .then((res)=>res.json())
